98094b92309ec7e24f81a9f26b7b7c370c4231ac
prasadtelkikar <telkikar.prasad@gmail.com>
2017-04-30 23:14:57 +0530

    Solved evaluate postfix expression; testing is remaining

---
+++ Chapter-12-Data-Structure/EvaluatePostfixExpression_12.13.C
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Node{
+	int data;
+	struct Node* nextNode;
+};
+
+int EvaluatePostfixExpression(char *expr);
+int Calculate(int op1, int op2, char oper);
+Node* Push(Node* top, int value);
+Node* Pop(Node* top);
+bool IsOperator(char ch);
+
+int main(){
+	char expression[50];
+	printf("\nEnter postfix expression: ");
+	scanf("%s", expression);
+	int result = EvaluatePostfixExpression(expression);
+	printf("Equation result: %d", result);
+	return 0;		
+}
+
+/* Evaluate Postfix expression */
+int EvaluatePostfixExpression(char *expression){
+	int i = 0;
+	Node* top = NULL;
+	int length = strlen(expression);
+	while(expression[i] != '\0'){
+		char ch = expression[i];
+		if(IsOperator(ch)){
+			int x = top -> data;
+			top = Pop(top);
+			int y = top -> data;
+			int result = Calculate(x, y, ch);
+			top = Push(top, result);
+		}
+		else{
+			int value = ch - '0';
+			top = Push(top, value);
+		}
+		i++;
+	}
+	return top -> data;
+}
+/*Push node at the top of the list. Principle: FILO*/
+Node* Push(Node* top, int value){
+	Node* temp = (Node*) malloc(sizeof(Node));
+	temp -> data = value;
+	temp -> nextNode = NULL;
+	if(top == NULL)
+		top = temp;
+	else{
+		temp -> nextNode = top;
+		top = temp;
+	}
+	return top;
+}
+/* Pop top node and replace head index to current top*/
+Node* Pop(Node* top){
+	top = top -> nextNode;
+	return top;
+}
+/* Check whether character ch is an operator*/
+bool IsOperator(char ch){
+	if(ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%')
+		return true;
+	else
+		return false;
+}
+
+/* Opearations on the basis of ch (operator)*/
+int Calculate(int op1, int op2, char ch){
+	int result = 0;
+	switch(ch){
+		case '+':
+			result = op1 + op2;
+		break;
+		case '-':
+		if(op1 > op2)
+			result = op1 - op2;
+		else
+			result = op2 - op1;
+		break;
+		case '*':
+			result = op1 * op2;
+		break;
+		case '/':
+		if(op1 > op2)
+			result = op1 / op2;
+		else
+			result = op2 / op1;
+		break;
+		case '%':
+		if(op1 > op2)
+			result = op1 % op2;
+		else
+			result = op2 % op1;
+		break;
+	}
+	return result;
+}
